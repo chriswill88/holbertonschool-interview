acbbd5b753be38dfdbaeeec6e353189f6328a112
chriswill88 <williechri79@gmail.com>
2020-12-10 22:07:31 -0500

    add all

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """This module contains the function for task 0"""
 
+
 def box_open(boxes, key, open_boxes, n, solution):
     """box_open - the recursive algorithm"""
-    if len(open_boxes) == len(boxes) or (0 not in open_boxes and len(open_boxes) + 1 == len(boxes)):
+    if len(open_boxes) == len(boxes) or (
+            0 not in open_boxes and len(open_boxes) + 1 == len(boxes)):
         return open_boxes
     for i in boxes[key]:
         if i not in open_boxes:
@@ -11,11 +13,15 @@ def box_open(boxes, key, open_boxes, n, solution):
     if solution:
         return solution
 
+
 def canUnlockAll(boxes):
     """
     canUnlockAll - checks if all boxes can be unlocked
     True if able to unlock all and false if not.
     """
+    if not isinstance(boxes, list):
+        return False
+
     if len(boxes) == 1:
         return True
     return True if box_open(boxes, 0, [], 1, None) else False
