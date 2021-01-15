2740575def10377d61f0cdff0ee9a4c178bb9817
chriswill88 <williechri79@gmail.com>
2021-01-14 23:53:02 -0500

    all

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -1,5 +1,22 @@
 #include "sandpiles.h"
 
+void tumbler(int grid[3][3], int row, int index)
+{
+    grid[row][index] -= 4;
+    // top
+    if (row-1 >= 0)
+        grid[row-1][index] += 1;
+    // bottom
+    if (row+1 < 3)
+        grid[row+1][index] += 1;
+    // left
+    if (index-1 >= 0)
+        grid[row][index-1] += 1;
+    // right
+    if (index+1 < 3)
+        grid[row][index+1] += 1;
+}
+
 int ischeck(int grid[3][3])
 {
     for (int i = 0; i < 3; i++)
@@ -29,26 +46,10 @@ void add(int grid1[3][3], int grid2[3][3])
             grid1[i][index] += grid2[i][index];
 }
 
-void tumbler(int grid[3][3], int row, int index)
-{
-    grid[row][index] -= 4;
-    // top
-    if (row-1 >= 0)
-        grid[row-1][index] += 1;
-    // bottom
-    if (row+1 < 3)
-        grid[row+1][index] += 1;
-    // left
-    if (index-1 >= 0)
-        grid[row][index-1] += 1;
-    // right
-    if (index+1 < 3)
-        grid[row][index+1] += 1;
-}
+
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int index = 0, row = 0;
 
     add(grid1, grid2);
     printgrid(grid1);
