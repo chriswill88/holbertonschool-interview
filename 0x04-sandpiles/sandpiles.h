d514aa986b01f5800245d367639551ca7d83ac7e
chriswill88 <williechri79@gmail.com>
2021-01-15 00:53:13 -0500

    all

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -6,7 +6,6 @@
 int ischeck(int grid[3][3]);
 void fix(int grid1[3][3], int grid2[3][3]);
 void printgrid(int grid[3][3]);
-void tumbler(int grid[3][3], int row, int index);
 void copygrid(int grid1[3][3], int grid2[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 
