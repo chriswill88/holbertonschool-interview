fa918342bb2575924da20c9b82e4d949ae9662b3
chriswill88 <williechri79@gmail.com>
2021-06-09 13:13:15 -0400

    [Completed task successfully]: compliled successful, executed successful

--- 0x18-merge_sort/0-merge_sort.c
+++ 0x18-merge_sort/0-merge_sort.c
@@ -1,25 +1,121 @@
 #include "sort.h"
 
-void working_sort(int *array, size_t subset) {
-    printf("subset size %ld\n", subset);
 
-    if (subset > 1)
-        working_sort(array, subset/2);
-
+/**
+ * printArray - prints segments of the array
+ * @A: array
+ * @iBegin: start index
+ * @iEnd: end index
+ *
+ * Return: void
+ */
+void printArray(int A[], size_t iBegin, size_t iEnd)
+{
+	for (; iBegin < iEnd; iBegin++)
+	{
+		printf("%d", A[iBegin]);
+		if (iBegin < iEnd - 1)
+			printf(", ");
+	}
 }
 
 
 /**
- * merge_sort - 
- * 
- * 
+ * TopDownMerge - sorts the segments of the array and merges them
+ * @B: working array
+ * @iBegin: start index
+ * @iMiddle: middle index
+ * @iEnd: end index
+ * @A: array
+ *
+ * Return: void
+ */
+void TopDownMerge(int A[], size_t iBegin, size_t iMiddle, size_t iEnd, int B[])
+{
+	size_t i = iBegin, j = iMiddle;
+
+	printf("[left]: ");
+	printArray(B, iBegin, iMiddle);
+	printf("\n");
+
+	printf("[right]: ");
+	printArray(B, iMiddle, iEnd);
+	printf("\n");
+
+	for (size_t k = iBegin; k < iEnd; k++)
+	{
+		if (i < iMiddle && (j >= iEnd || A[i] <= A[j]))
+		{
+			B[k] = A[i];
+			i = i + 1;
+		}
+		else
+		{
+			B[k] = A[j];
+			j = j + 1;
+		}
+	}
+	printf("[Done]: ");
+	printArray(B, iBegin, iEnd);
+	printf("\n");
+}
+
+
+/**
+ * TopDownSplitMerge - this function recursivly splits the array then merges
+ * @B: working array
+ * @iBegin: start index
+ * @iEnd: end index
+ * @A: array
+ *
+ * Return: void
+ */
+void TopDownSplitMerge(int B[], size_t iBegin, size_t iEnd, int A[])
+{
+	if (iEnd - iBegin <= 1)
+		return;
+	size_t iMiddle = (iEnd + iBegin) / 2;
+
+	TopDownSplitMerge(A, iBegin,  iMiddle, B);
+	TopDownSplitMerge(A, iMiddle,    iEnd, B);
+	printf("Merging...\n");
+	TopDownMerge(B, iBegin, iMiddle, iEnd, A);
+}
+
+
+/**
+ * TopDownMergeSort - copies the array into a working buffer, starts the algo
+ * @A: is the array
+ * @B: is the working array
+ * @n: is that size of the array
+ *
+ * Return: void
+ */
+void TopDownMergeSort(int A[], int B[], size_t n)
+{
+	for (size_t k = 0; k < n; k++)
+		B[k] = A[k];
+
+	TopDownSplitMerge(B, 0, n, A);
+}
+
+
+/**
+ * merge_sort - sorts an array using the merge sort algorithm
+ * @array: the array we will sort
+ * @size: the size of the array
+ *
+ * Return: void
  */
 void merge_sort(int *array, size_t size)
 {
-    int *workarray = malloc(size);
+	int *workarray = malloc(size);
 
-    printf("Start merge sort\n");
-    free(workarray);
+	if (!workarray)
+		return;
 
-    working_sort(array, size);
+	printf("Start merge sort\n");
+
+	TopDownMergeSort(array, workarray, size);
+	free(workarray);
 }
