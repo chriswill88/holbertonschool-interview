a83b194d92d7635993cbc769bdb07e1a762196f3
chriswill88 <williechri79@gmail.com>
2021-06-09 13:22:37 -0400

    [added functions declarations to sort.h]

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -6,6 +6,11 @@
 
 void print_array(const int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void printArray(int A[], size_t iBegin, size_t iEnd);
+void TopDownMerge(int A[], size_t iBegin, size_t iMiddle, size_t iEnd, int B[]);
+void TopDownSplitMerge(int B[], size_t iBegin, size_t iEnd, int A[]);
+void TopDownMergeSort(int A[], int B[], size_t n);
+
 
 
 #endif
