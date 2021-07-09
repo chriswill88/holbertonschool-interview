ed23155750955694b99975090bf2e5fd0c81a76f
chriswill88 <williechri79@gmail.com>
2021-07-09 00:42:59 -0400

    done

--- 0x1B-radix_sort/0-radix_sort.c
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -1,70 +1,113 @@
 #include "sort.h"
 
+/**
+ * copyArr - copy array
+ *
+ * @array: the array
+ * @ptr: the array to copy
+ * @size: the size of the array
+ * Return: Void
+ */
 void copyArr(int *array, int *ptr, size_t size)
 {
-    size_t i = 0;
+	size_t i = 0;
 
-    for (; i < size; i++)
-        array[i] = ptr[i];
+	for (; i < size; i++)
+		array[i] = ptr[i];
 }
 
+/**
+ * maxer - gets the max
+ *
+ * @array: the array
+ * @size: the size of the array
+ * Return: Void
+ */
 int maxer(int *array, size_t size)
 {
-    size_t i = 0;
-    int max = 0;
+	size_t i = 0;
+	int max = 0;
 
-    for (; i < size; i++)
-        if (array[i] > max)
-            max = array[i];
+	for (; i < size; i++)
+		if (array[i] > max)
+			max = array[i];
 
-    return max;
+	return (max);
 }
 
-
+/**
+ * indexer - spliter function
+ *
+ * @ele: the element from the array
+ * @placer: the placer
+ * @index: the index
+ * Return: Void
+ */
 int indexer(int ele, int index, int placer)
 {
-    return (ele % index) / placer;
+	return ((ele % index) / placer);
 }
 
+/**
+ * sort - sorts the array
+ *
+ * @array: the array
+ * @ptr: the manipulated pointer
+ * @max: the max
+ * @index: the index
+ * @placer: the placer
+ * @size: the size of the array
+ * Return: Void
+ */
 void sort(int *array, int *ptr, int max, int index, int placer, size_t size)
 {
-    size_t i;
-    int x = 0, y = 0;
+	size_t i;
+	int x = 0, y = 0;
 
-    if (index > max * 10)
-        return ;
+	if (index > max * 10)
+		return;
 
-    for (; x < 10; x++)
-    {
-        for (i = 0; i < size; i++)
-        {
-            if (x == indexer(array[i], index, placer))
-            {
-                ptr[y] = array[i];
-                y++;
-            }
-        }
-    }
-    copyArr(array, ptr, size);
-    print_array(array, size);
+	for (; x < 10; x++)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (x == indexer(array[i], index, placer))
+			{
+				ptr[y] = array[i];
+				y++;
+			}
+		}
+	}
+	copyArr(array, ptr, size);
+	print_array(array, size);
 
-    sort(array, ptr, max, index * 10, placer * 10, size);
+	sort(array, ptr, max, index * 10, placer * 10, size);
 }
 
+/**
+ * radix_sort - sorts the array
+ *
+ * @array: the array
+ * @size: size of the array
+ * Return: void
+ */
 void radix_sort(int *array, size_t size)
 {
-    size_t i = 0;
-    int max, *ptr;
+	size_t i = 0;
+	int max, *ptr;
 
-    ptr = malloc(size * sizeof(int));
-    if (ptr == NULL)
-        return ;
-    
-    for (; i < size; i++)
-        ptr[i] = 0;
+	if (!array)
+		return (NULL);
 
-    max = maxer(array, size);
-    
-    sort(array, ptr, max, 10, 1, size);
-    free(ptr);
+	ptr = malloc(size * sizeof(int));
+	if (ptr == NULL)
+		return;
+
+	for (; i < size; i++)
+		ptr[i] = 0;
+
+	max = maxer(array, size);
+
+	sort(array, ptr, max, 10, 1, size);
+	free(ptr);
 }
--- 0x1B-radix_sort/sort.h
+++ 0x1B-radix_sort/sort.h
@@ -4,6 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void copyArr(int *array, int *ptr, size_t size);
+int maxer(int *array, size_t size);
+int indexer(int ele, int index, int placer);
+void sort(int *array, int *ptr, int max, int index, int placer, size_t size);
 void radix_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 
