dc2ccf347ad69ad3f3c206127c7ae890461c18cd
chriswill88 <williechri79@gmail.com>
2021-07-14 16:57:19 -0400

    verbose try and except

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -10,11 +10,24 @@ def island_perimeter(grid):
 
     for row in range(len(grid)):
         for ele in range(len(grid[row])):
-            if 0 < row < len(grid) - 1 and 0 < ele < len(grid[row]) - 1: 
-                if grid[row][ele] == 1:
+            if grid[row][ele] == 1:
+                try:
                     sumate += 1 if grid[row][ele - 1] == 0 else 0
+                except Exception:
+                    pass
+
+                try:
                     sumate += 1 if grid[row - 1][ele] == 0 else 0
+                except Exception:
+                    pass
+
+                try:
                     sumate += 1 if grid[row + 1][ele] == 0 else 0
+                except Exception:
+                    pass
+
+                try:
                     sumate += 1 if grid[row][ele + 1] == 0 else 0
-                    print(sumate)
+                except Exception:
+                    pass
     return sumate
