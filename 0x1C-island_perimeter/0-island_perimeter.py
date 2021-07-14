63eb8473b3a90b2b82419ad3fdbd019577c66229
chriswill88 <williechri79@gmail.com>
2021-07-14 17:46:08 -0400

    better vars

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -11,9 +11,10 @@ def island_perimeter(grid):
     for row in range(rows):
         for ele in range(eles):
             if grid[row][ele] == 1 and 0 < row < rows and 0 < ele < eles:
-                sumate += 1 if grid[row + 1][ele] == 0 else 0
-                sumate += 1 if grid[row - 1][ele] == 0 else 0
-                sumate += 1 if grid[row][ele + 1] == 0 else 0
-                sumate += 1 if grid[row][ele - 1] == 0 else 0
+                sumate += 4
+                sumate -= 1 if grid[row + 1][ele] != 0 else 0
+                sumate -= 1 if grid[row - 1][ele] != 0 else 0
+                sumate -= 1 if grid[row][ele + 1] != 0 else 0
+                sumate -= 1 if grid[row][ele - 1] != 0 else 0
 
     return sumate
