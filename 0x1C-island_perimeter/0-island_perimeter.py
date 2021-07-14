26714547f457ec9a0f4cca9d9de051dbfdbcc480
chriswill88 <williechri79@gmail.com>
2021-07-14 16:46:42 -0400

    cleaner way

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -10,21 +10,10 @@ def island_perimeter(grid):
 
     for row in range(len(grid)):
         for ele in range(len(grid[row])):
-            if grid[row][ele] == 1:
-                try:
+            if 0 < row < len(grid) - 1 and 0 < ele < len(grid[row]) - 1: 
+                if grid[row][ele] == 1:
                     sumate += 1 if grid[row][ele - 1] == 0 else 0
-                except Exception:
-                    pass
-                try:
                     sumate += 1 if grid[row - 1][ele] == 0 else 0
-                except Exception:
-                    pass
-                try:
                     sumate += 1 if grid[row + 1][ele] == 0 else 0
-                except Exception:
-                    pass
-                try:
                     sumate += 1 if grid[row][ele + 1] == 0 else 0
-                except Exception:
-                    pass
     return sumate
