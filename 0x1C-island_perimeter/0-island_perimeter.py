5a06266c908f358636e836b7f570609ee7d54417
chriswill88 <williechri79@gmail.com>
2021-07-14 17:21:36 -0400

    removed try and except

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -4,30 +4,17 @@
 
 def island_perimeter(grid):
     """this function gets the perimeter of the island made of arrays"""
-    sumate = 0
-    if len(grid) < 3:
-        return sumate
+    summer = 0
 
     for row in range(len(grid)):
         for ele in range(len(grid[row])):
             if grid[row][ele] == 1:
-                try:
+                sumate = 0
+                if 0 < row < len(grid) - 1 and 0 < ele < len(grid[row]) - 1:
                     sumate += 1 if grid[row][ele - 1] == 0 else 0
-                except Exception:
-                    pass
-
-                try:
-                    sumate += 1 if grid[row - 1][ele] == 0 else 0
-                except Exception:
-                    pass
-
-                try:
-                    sumate += 1 if grid[row + 1][ele] == 0 else 0
-                except Exception:
-                    pass
-
-                try:
                     sumate += 1 if grid[row][ele + 1] == 0 else 0
-                except Exception:
-                    pass
-    return sumate
+                    sumate += 1 if grid[row - 1][ele] == 0 else 0
+                    sumate += 1 if grid[row + 1][ele] == 0 else 0
+                    summer += sumate
+
+    return summer
