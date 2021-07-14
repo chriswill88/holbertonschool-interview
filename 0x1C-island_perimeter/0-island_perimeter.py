3bd47798afa485c6e49a87ffad39cef7b139fa21
chriswill88 <williechri79@gmail.com>
2021-07-14 16:12:26 -0400

    island check

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -2,26 +2,11 @@
 """This module contains an interview solution"""
 
 
-def bounderyCheck(grid):
-    sumL = 0
-    sumR = 0
-
-    if sum(grid[0]) != 0:
-        return False
-    if sum(grid[len(grid) - 1]) != 0:
-        return False
-
-    for i in range(len(grid)):
-        sumL += grid[i][0]
-        sumR += grid[i][len(grid)]
-    return True if sumL + sumR == 0 else False
-    
-
 def island_perimeter(grid):
     """this function gets the perimeter of the island made of arrays"""
     sumate = 0
-    if bounderyCheck(grid) == False:
-        return 0
+    if len(grid) < 3:
+        return sumate
 
     for row in range(len(grid)):
         for ele in range(len(grid[row])):
