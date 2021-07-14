a33742432d1f41acd070b9297ff5a85098650e1b
chriswill88 <williechri79@gmail.com>
2021-07-14 15:22:14 -0400

    Done

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module contains an interview solution"""
+
+
+def island_perimeter(grid):
+    """this function gets the perimeter of the island made of arrays"""
+    sumate = 0
+    for row in range(len(grid)):
+        for ele in range(len(grid[row])):
+            if grid[row][ele] == 1:
+                try:
+                    sumate += 1 if grid[row][ele - 1] == 0 else 0
+                    sumate += 1 if grid[row - 1][ele] == 0 else 0
+                    sumate += 1 if grid[row + 1][ele] == 0 else 0
+                    sumate += 1 if grid[row][ele + 1] == 0 else 0
+                except Exception:
+                    pass
+    return sumate
