d892586439110b215c0dbb74a69e15a6e96fab71
chriswill88 <williechri79@gmail.com>
2021-07-18 13:59:06 -0400

    added constraints to number of rows and elements

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -10,7 +10,7 @@ def island_perimeter(grid):
 
     for row in range(rows):
         for ele in range(eles):
-            if grid[row][ele] == 1 and 0 < row < rows and 0 < ele < eles:
+            if grid[row][ele] == 1 and 0 < row < rows - 1 and 0 < ele < eles - 1:
                 sumate += 1 if grid[row + 1][ele] == 0 else 0
                 sumate += 1 if grid[row - 1][ele] == 0 else 0
                 sumate += 1 if grid[row][ele + 1] == 0 else 0
