ea5311352f308a31305ace4b8b81cdf6abf4d439
chriswill88 <williechri79@gmail.com>
2021-07-18 14:16:45 -0400

    removed constraits

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -10,21 +10,26 @@ def island_perimeter(grid):
 
     for row in range(rows):
         for ele in range(eles):
-            if grid[row][ele] == 1 and 0 < row < rows and 0 < ele < eles:
+            if grid[row][ele] == 1:
+              sumate += 4
+              # down
               try:
-                sumate += 1 if grid[row + 1][ele] == 0 else 0
+                sumate -= 1 if grid[row + 1][ele] == 1 else 0
               except Exception:
-                sumate += 1
+                pass
+              # up
               try:
-                sumate += 1 if grid[row - 1][ele] == 0 else 0
+                sumate -= 1 if grid[row - 1][ele] == 1 else 0
               except Exception:
-                sumate += 1
+                pass
+              # right
               try:
-                sumate += 1 if grid[row][ele + 1] == 0 else 0
+                sumate -= 1 if grid[row][ele + 1] == 1 else 0
               except Exception:
-                sumate += 1
+                pass
+              # left
               try:
-                sumate += 1 if grid[row][ele - 1] == 0 else 0
+                sumate -= 1 if grid[row][ele - 1] == 1 else 0
               except Exception:
-                sumate += 1
+                pass
     return sumate
