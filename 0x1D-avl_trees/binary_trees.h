5f09db5a0b6f0c8a963d525430c82c5daf383668
chriswill88 <williechri79@gmail.com>
2021-08-02 01:53:20 -0400

    [ADD task 0] betty checked out

--- 0x1D-avl_trees/binary_trees.h
+++ 0x1D-avl_trees/binary_trees.h
@@ -15,17 +15,17 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
 
 typedef struct binary_tree_s avl_t;
 int binary_tree_is_avl(const binary_tree_t *);
-static int print_t(const binary_tree_t *tree, int , int , char **);
+static int print_t(const binary_tree_t *tree, int, int, char **);
 static size_t _height(const binary_tree_t *);
 void binary_tree_print(const binary_tree_t *);
 
