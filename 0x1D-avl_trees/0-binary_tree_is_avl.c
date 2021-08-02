3dffc4b76036a3011f4869386fb739be13f405a0
chriswill88 <williechri79@gmail.com>
2021-08-02 01:51:46 -0400

    [ADD task 0]

--- 0x1D-avl_trees/0-binary_tree_is_avl.c
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -1,21 +1,23 @@
 #include "binary_trees.h"
+
 /**
  * height - returns the height of the binary tree
  * @tree: ptr to the tree
- * 
+ *
  * Return: the height of the tree.
  */
-int height(binary_tree_t* tree) {
+int height(binary_tree_t *tree)
+{
 	if (tree == NULL)
 		return (-1);
 	else
-		return max(height(tree->left), height(tree->right)) + 1;
+		return (max(height(tree->left), height(tree->right)) + 1);
 }
 
 /**
  * binary_tree_is_avl - checks if a tree is avl
  * @tree: tree head pointer
- * 
+ *
  * Return: 1 if true 0 if false
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
