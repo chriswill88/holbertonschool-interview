29d232cab7dfdc4214bd201da938395264edbf4a
chriswill88 <williechri79@gmail.com>
2021-08-08 05:28:31 +0000

    [fix] task 0

--- 0x1D-avl_trees/0-binary_tree_is_avl.c
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -1,18 +1,24 @@
 #include "binary_trees.h"
 
-
+/**
+ * is_sort - checks if the node is in the right place
+ *
+ * @head: head of the tree
+ * @node: node to check for
+ * Return: 0 || 1
+ */
 int is_sort(binary_tree_t *head, binary_tree_t *node)
 {
-  int hval, nval = node->n;
+	int hval, nval = node->n;
 
-  if (head == NULL)
-    return (0);
-  hval = head->n;
+	if (head == NULL)
+		return (0);
+	hval = head->n;
 
-  if (hval == nval)
-    return (1);
- 
-  return (nval > hval ? is_sort(head->right, node) : is_sort(head->left, node));
+	if (hval == nval)
+		return (1);
+
+	return (nval > hval ? is_sort(head->right, node) : is_sort(head->left, node));
 }
 
 /**
@@ -29,17 +35,19 @@ int max(int num1, int num2)
 
 /**
  * height - returns the height of the binary tree
+ *
  * @tree: ptr to the tree
+ * @h: head of the tree
  *
  * Return: the height of the tree.
  */
-int height(binary_tree_t *tree, binary_tree_t* head)
+int height(binary_tree_t *tree, binary_tree_t *h)
 {
-  if (tree && !is_sort(head, tree))
-  {
-    return (100000);
-  }
-	return (!tree ? 0 : (max(height(tree->left, head), height(tree->right, head)) + 1));
+	if (tree && !is_sort(h, tree))
+	{
+		return (100000);
+	}
+	return (!tree ? 0 : (max(height(tree->left, h), height(tree->right, h)) + 1));
 }
 
 /**
@@ -50,5 +58,10 @@ int height(binary_tree_t *tree, binary_tree_t* head)
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-	return ((!tree || (height(tree->right, (binary_tree_t *)tree) - height(tree->left, (binary_tree_t *)tree)) > 1) ? 0 : 1);
+	binary_tree_t *h = (binary_tree_t *)tree;
+
+	if (!tree)
+		return (0);
+
+	return (((height(tree->right, h) - height(tree->left, h)) > 1) ? 0 : 1);
 }
