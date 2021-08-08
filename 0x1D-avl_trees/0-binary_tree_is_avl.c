2692ac1c5aa3e34082f2a67a36e7297688f4051f
chriswill88 <williechri79@gmail.com>
2021-08-08 05:14:53 +0000

    [fix] task 0

--- 0x1D-avl_trees/0-binary_tree_is_avl.c
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -1,5 +1,20 @@
 #include "binary_trees.h"
 
+
+int is_sort(binary_tree_t *head, binary_tree_t *node)
+{
+  int hval, nval = node->n;
+
+  if (head == NULL)
+    return (0);
+  hval = head->n;
+
+  if (hval == nval)
+    return (1);
+ 
+  return (nval > hval ? is_sort(head->right, node) : is_sort(head->left, node));
+}
+
 /**
  * max - returns the greater of two numbers
  *
@@ -18,9 +33,13 @@ int max(int num1, int num2)
  *
  * Return: the height of the tree.
  */
-int height(binary_tree_t *tree)
+int height(binary_tree_t *tree, binary_tree_t* head)
 {
-	return (!tree ? 0 : (max(height(tree->left), height(tree->right)) + 1));
+  if (tree && !is_sort(head, tree))
+  {
+    return (100000);
+  }
+	return (!tree ? 0 : (max(height(tree->left, head), height(tree->right, head)) + 1));
 }
 
 /**
@@ -31,5 +50,5 @@ int height(binary_tree_t *tree)
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-	return ((!tree || (height(tree->right) - height(tree->left)) > 1) ? 0 : 1);
+	return ((!tree || (height(tree->right, (binary_tree_t *)tree) - height(tree->left, (binary_tree_t *)tree)) > 0) ? 0 : 1);
 }
