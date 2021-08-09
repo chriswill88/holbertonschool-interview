3a08f6a3bbdb67628995bf9e7f8679cf4d5c4ccb
chriswill88 <williechri79@gmail.com>
2021-08-09 03:11:30 +0000

    [add task]

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""this module contains the function to task 0"""
+
+
+def pascals_rows(n):
+  """this creates the rows for pascals triangle"""
+  start = [1]
+
+  if len(n) == 0:
+    return [1]
+
+  for i in range(len(n)):
+    if i + 1 in range(len(n)):
+      value = sum(n[i: i+2])
+    else:
+      value = n[i]
+    start.append(value)
+    
+  return start
+
+
+def pascal_triangle(n):
+  """gives the rows in the pascal_triangle"""
+  triangle = []
+  row = []
+
+  for i in range(n):
+    row = pascals_rows(row)
+    triangle.append(row)
+  return triangle
