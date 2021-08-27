299af6998e3ac48c8ede3e474ce3797b53906f09
chriswill88 <williechri79@gmail.com>
2021-08-27 05:44:43 -0400

    [add task]

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -6,10 +6,7 @@ def is_prime(n):
     """
       this function checks if the number is a prime
     """
-    if n <= 1:
-        return 0
-
-    for i in range(2, int(n / 2)):
+    for i in range(int(n**(1 / 2)), int(n / 2)):
         if not (n % i):
             return 0
     return 1
@@ -22,6 +19,8 @@ def find_multiples(array, num):
     """
     for i in array:
         if i % num == 0:
+            if (num == i and is_prime(i)):
+                continue
             array.remove(i)
 
 
@@ -29,15 +28,26 @@ def game(array, winner):
     """
       this function recursivly plays the prime game
     """
-
-    while len(array):
-        value = array[0]
-        if is_prime(value):
-            find_multiples(array, value)
-            winner = 'Maria' if winner == 'Ben' else 'Ben'
-        else:
-            find_multiples(array, value)
+    for _ in array:
+        winner = 'Maria' if winner == 'Ben' else 'Ben'
     return winner
+
+
+def list_maker(array):
+    """
+      this function recursivly plays the prime game
+    """
+    i = 0
+    while i < len(array):
+        value = array[i]
+        find_multiples(array, value)
+        i += 1
+
+    return array
+
+
+def get_relevant_primes(primes, num):
+    return [i for i in primes if i <= num]
 
 
 def isWinner(x, nums):
@@ -48,9 +58,10 @@ def isWinner(x, nums):
     winner = []
     if x < 1:
         return None
+    primes = list_maker(list(range(2, max(nums) + 1)))
 
     for i in nums:
-        winner.append(game(list(range(2, i + 1)), 'Ben'))
+        winner.append(game(get_relevant_primes(primes, i), 'Ben'))
     if winner.count('Ben') == winner.count('Maria'):
         return None
     return 'Ben' if winner.count('Ben') > winner.count('Maria') else 'Maria'
